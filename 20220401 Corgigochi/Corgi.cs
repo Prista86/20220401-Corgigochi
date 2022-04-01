51f750bac033e4f02db2684673296786acc431df
LukePC <LukePC@DESKTOP-SMI2KCQ>
2022-04-01 23:27:38 +0200

    Impostato F T A D

---
+++ 20220401 Corgigochi/Corgi.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220401_Corgigochi
+{
+    class Corgi
+    {
+        public string Nome { get; set; }
+        public int Felice { get; set; }
+        public int Triste { get; set; }
+        public int Arrabbiato { get; set; }
+        public int Depresso { get; set; }
+        public int Dormire { get; set; }
+        public int Mangiare { get; set; }
+        public int Coccole { get; set; }
+        public int Pulire { get; set; }
+        public int ProbDormire { get; set; }
+        public int ProbMangiare { get; set; }
+        public int ProbCoccole { get; set; }
+        public int ProbPulire { get; set; }
+        public string Necessita { get; set; }
+
+
+        //AZIONI giocatore
+        public void FaiDormire()
+        {            
+            this.ProbDormire = 0;
+            this.Dormire = this.ProbDormire;
+        }
+        public void FaiMangiare()
+        {            
+            this.ProbMangiare -= 3;
+            this.Mangiare = this.ProbMangiare;
+        }
+        public void Pulisci()
+        {   
+            this.ProbPulire -= 5;
+            this.Pulire = this.ProbPulire;
+        }
+        public void Coccolare()
+        {             
+            this.ProbCoccole -= 4;
+            this.Coccole = this.ProbCoccole;
+        }
+        public void FaiNulla()
+        {
+
+        }
+        public void AzioniPratica()
+        {
+            Console.SetCursorPosition(35, 18);
+            string azione = Console.ReadLine();
+            bool n = int.TryParse(azione, out int azioneNum);
+            switch (azioneNum)
+            {
+                case 1:
+                    FaiDormire();
+                    break;
+                case 2:
+                     FaiMangiare();
+                    break;
+                case 3:
+                    Coccolare();
+                    break;
+                case 4:
+                    Pulisci();
+                    break;
+                case 5:
+                    FaiNulla();
+                    break;
+                default:
+                    Console.WriteLine("Devi inserire un numero da 1 a 5");
+                    break;
+            }
+        }
+
+        //Azioni Corgi
+        public void VoglioDormire()
+        {
+            Console.SetCursorPosition(50, 9);
+            this.Necessita = "Voglio dormire";
+            Console.WriteLine(this.Necessita);
+            this.ProbDormire++;
+            this.Dormire = this.ProbDormire;
+            StampaCorgi();
+            AzioniPratica();
+        }
+        public void VoglioMangiare()
+        {
+            Console.SetCursorPosition(49, 9);
+            this.Necessita = "Voglio Mangiare";
+            Console.WriteLine(this.Necessita);
+            this.ProbMangiare+=2;
+            this.Mangiare = this.ProbMangiare;
+            StampaCorgi();
+            AzioniPratica();
+        }
+        public void DeviPulire()
+        {
+            Console.SetCursorPosition(50, 9);
+            this.Necessita = " Devi pulire ";
+            Console.WriteLine(this.Necessita);
+            this.ProbPulire+=3;
+            this.Pulire = this.ProbPulire;
+            StampaCorgi();
+            AzioniPratica();
+        }
+        public void VoglioCoccole()
+        {
+            Console.SetCursorPosition(50, 9);
+            this.Necessita = "Voglio coccole";
+            Console.WriteLine(this.Necessita);
+            this.ProbCoccole++;
+            this.Coccole = this.ProbCoccole;
+            StampaCorgi();
+            AzioniPratica();
+        }
+
+        public void StampaCorgi()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(30, 1);
+            Console.WriteLine($@"            |¯¯¯¯|                   |¯¯¯¯|");
+            Console.SetCursorPosition(30, 2);
+            Console.WriteLine($@"Felice      | {this.Felice}  |      {this.Nome}     | {this.Dormire}  |  Dormire");
+            Console.SetCursorPosition(30, 3);
+            Console.WriteLine($@"            |----|                   |----|");
+            Console.SetCursorPosition(30, 4);
+            Console.WriteLine($@"Triste      | {this.Triste}  |     ,-.___,-.     | {this.Mangiare}  |  Mangiare");
+            Console.SetCursorPosition(30, 5);
+            Console.WriteLine($@"            |----|     \_/_ _\_/     |----|");
+            Console.SetCursorPosition(30, 6);
+            Console.WriteLine($@"Arrabbiato  | {this.Arrabbiato}  |       )O_O(       | {this.Coccole}  |  Coccole");
+            Console.SetCursorPosition(30, 7);
+            Console.WriteLine(@"            |----|      { (_) }      |----|");
+            Console.SetCursorPosition(30, 8);
+            Console.WriteLine($@"Depresso    | {this.Depresso}  |       `-^-'       | {this.Pulire}  |  Pulire");
+            Console.SetCursorPosition(30, 9);
+            Console.WriteLine($@"            |____|                   |____|");
+            Console.WriteLine($@"                                                  {this.Necessita}");
+            Console.WriteLine();
+            Console.SetCursorPosition(35, 12);
+            Console.WriteLine("Vuoi eseguire una di queste Azioni?");
+            Console.SetCursorPosition(35, 13);
+            Console.WriteLine("1. Mandalo a dormire.");
+            Console.SetCursorPosition(35, 14);
+            Console.WriteLine("2. Dargli da mangiare.");
+            Console.SetCursorPosition(35, 15);
+            Console.WriteLine("3. Fagli le coccole.");
+            Console.SetCursorPosition(35, 16);
+            Console.WriteLine("4. Pulisci la cacca.");
+            Console.SetCursorPosition(35, 17);
+            Console.WriteLine("5. Nono fare nulla.");
+            //Console.ReadKey();
+        }
+
+    }
+}
--- 20220401 Corgigochi/Program.cs
+++ 20220401 Corgigochi/Program.cs
@@ -2,40 +2,111 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+//Problemi:
+    // Come utilizzare una funzioen dentro una classe
+    // come mettere un ReadLine con scadenza temporale
 
 namespace _20220401_Corgigochi
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            string giocatore;
-            string nomeCorgi;
-            Console.WriteLine("Ciao! Come ti chiami???");
-            giocatore = Console.ReadLine();
-            Console.WriteLine($"Oggi {giocatore} è proprio un giorno speciale!!");
-            Console.WriteLine("Hai ottenuto il tuo primo Corgimon!!!");
-            Console.ReadKey();
-            Console.WriteLine("Che nome gli vuoi dare");
-            nomeCorgi = Console.ReadLine();
-            Console.WriteLine($"{nomeCorgi} è veramente un bel nome!!");
-            Console.ReadKey();
-            StampaCorgi(nomeCorgi);
+            string giocatore = "Luca";
+            int numero = 1;
+            int dormire = numero;
+            int mangiare = numero;
+            int pulire = numero;
+            int coccole = numero;
 
-        }
-        static void StampaCorgi(string nomeCorgi)
-        {
-            Console.WriteLine($"   {nomeCorgi}    ");
-            Console.WriteLine("");
-            Console.WriteLine(@"  ,-.___,-.   ");
-            Console.WriteLine(@"  \_/_ _\_/   ");
-            Console.WriteLine(@"    )O_O(     ");
-            Console.WriteLine(@"   { (_) }    ");
-            Console.WriteLine(@"    `-^-'     ");
-            Console.ReadKey();
+            Corgi corgi = new Corgi();
+            corgi.Nome = "Roosvelt";
+            corgi.Felice = 0;
+            corgi.Triste = 0;
+            corgi.Arrabbiato = 0;
+            corgi.Depresso = 0;
+            corgi.Dormire = 0;
+            corgi.Mangiare = 0;
+            corgi.Coccole = 0;
+            corgi.Pulire = 0;
 
 
+            //Console.WriteLine("Ciao! Come ti chiami???");
+            //giocatore = Console.ReadLine();
+            //Console.WriteLine($"Oggi {giocatore} è proprio un giorno speciale!!");
+            //Console.WriteLine("Hai ottenuto il tuo primo Corgimon!!!");
+            //Console.ReadKey();
+            //Console.WriteLine("Che nome gli vuoi dare?");
+            //nomeCorgi = Console.ReadLine();
+            //Console.WriteLine($"{corgi.Nome} è veramente un bel nome!!");
+            //Console.ReadKey();
+            //StampaCorgi(corgi.Nome);
+            //Console.WriteLine("Tra poco inizierà la tua nuova avventura!!!");
+            //Console.ReadKey();
+
+            //for (int i = 3; i > 0; i--)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine(i);
+            //    Thread.Sleep(1000);
+            //}           
+
+            while (true)
+            {
+                dormire--;
+                mangiare--;
+                pulire--;
+                coccole--;
+                if (dormire == 0)
+                {
+                    corgi.StampaCorgi();
+                    corgi.VoglioDormire();
+                    dormire = numero;
+                }
+                
+                if (mangiare == 0)
+                {
+                    corgi.StampaCorgi();
+                    corgi.VoglioMangiare();                    
+                    mangiare = numero;
+                }
+                if (pulire == 0)
+                {
+                    corgi.StampaCorgi();
+                    corgi.DeviPulire();                   
+                    pulire = numero;
+                }
+                if (coccole == 0)
+                {
+                    corgi.StampaCorgi();
+
+                    corgi.VoglioCoccole();
+                    coccole = numero;
+                }
+
+                if (corgi.Dormire + corgi.Mangiare+corgi.Pulire+corgi.Coccole < 10)
+                {
+                    corgi.Felice = 99;
+                }
+                if (corgi.Dormire + corgi.Mangiare + corgi.Pulire + corgi.Coccole > 10)
+                {
+                    corgi.Triste = 99;
+                }
+                if (corgi.Dormire + corgi.Mangiare + corgi.Pulire + corgi.Coccole > 20)
+                {
+                    corgi.Arrabbiato = 99;
+                }
+                if (corgi.Dormire + corgi.Mangiare + corgi.Pulire + corgi.Coccole > 30)
+                {
+                    corgi.Depresso = 99;
+                }
+                Console.Clear();
+                Console.SetCursorPosition(25, 17);
+                //Thread.Sleep(1000);
+            }
         }
     }
 }
